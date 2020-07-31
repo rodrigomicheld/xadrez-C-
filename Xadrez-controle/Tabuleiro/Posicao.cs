ab7329928b860bae54ada1b412956bf327961ecb
Rodrigo Michel <rodrigo.micheld@gmail.com>
2020-07-30 21:25:08 -0300

    Criando a classe posição

---
+++ Xadrez-controle/Tabuleiro/Posicao.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Tabuleiro {
+    class Posicao {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna) {
+            Linha = linha;
+            Coluna = coluna;
+        }
+    }
+}
