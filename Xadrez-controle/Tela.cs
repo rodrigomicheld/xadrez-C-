b25b91c1a87c9e5ec56eab65e9b15d55fb6e4a44
Rodrigo Michel <rodrigo.micheld@gmail.com>
2020-08-08 23:06:02 -0300

    Melhorandoa aimplementação método imprimirTabuleiro

--- Xadrez-controle/Tela.cs
+++ Xadrez-controle/Tela.cs
@@ -8,6 +8,7 @@ namespace Xadrez_controle {
         public static void imprimirTabuleiro(Tabuleiro tab) {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -16,12 +17,29 @@ namespace Xadrez_controle {
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
 
 
                 }
                 Console.WriteLine();
+            }
+            Console.Write("  a b c d e f g h");
+
+
+        }
+        public static void imprimirPeca(Peca peca) {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
